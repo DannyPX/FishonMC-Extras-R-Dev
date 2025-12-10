e7f76abd932801ab356d5a5afd8d23e8a7db6d91
DannyPX <47347980+DannyPX@users.noreply.github.com>
2025-12-10 16:10:56 +0100

    Init

---
+++ src/main/java/dannypx/foe/FishOnMCExtrasClient.java
@@ -0,0 +1,11 @@
+package dannypx.foe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishOnMCExtrasClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
