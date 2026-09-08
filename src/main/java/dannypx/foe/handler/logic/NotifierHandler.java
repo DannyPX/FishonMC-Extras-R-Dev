9f4bdcc9e6d959b1a40277b2532d0e3011028345
DannyPX <47347980+DannyPX@users.noreply.github.com>
2026-09-08 13:37:20 +0200

    fix: line doesnt hide when there is a false positive error, hide line should be first prio

--- src/main/java/dannypx/foe/handler/logic/NotifierHandler.java
+++ src/main/java/dannypx/foe/handler/logic/NotifierHandler.java
@@ -374,7 +374,7 @@ public class NotifierHandler extends Handler {
 
                 List<MutableComponent> lines = notification.getStringLines().stream()
                         .map(PlaceholderHandlerV2.instance()::resolve)
-                        .filter(result -> (result.success()[0] && !result.success()[1]) || !result.errors().isEmpty())
+                        .filter(result -> (result.success()[0] || !result.errors().isEmpty()) && !result.success()[1])
                         .map(PlaceholderResult::text).toList();
                 List<Component> newLines = new ArrayList<>();
 
